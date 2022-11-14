458310897ec1eeb23eb6186690f79e0ea7921e13
SteshM <mbithestella0@gmail.com>
2022-11-15 01:02:17 +0300

    Product update

--- app.py
+++ app.py
@@ -75,6 +75,23 @@ def get_product(id):
    product = Product.query.get(id)
    return product_schema.jsonify(product)
 
+   #update a product
+@app.route ('/product/<id>', methods=['PUT'])
+def update_product(id):
+   product = Product.query.get(id)
+   name = request.json['name']
+   description = request.json['description']
+   price = request.json['price']
+   qty = request.json['qty']
+
+   product.name = name
+   product.description = description
+   product.price = price
+   product.qty = qty
+
+   db.session.commit()
+   return product_schema.jsonify(product)
+
 
      # Run Server
 if __name__== '_main_':
